ef0d68540b2251ab93afde2222cfdfb96dc32e29
LiYang <lyang.1915@gmail.com>
2017-08-13 08:46:09 +0800

    repair some BUG in Fuyufeng's code

--- Python/2nd_Lesson/Wu Yufeng/Rpg_main.py
+++ Python/2nd_Lesson/Wu Yufeng/Rpg_main.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 import time
 class Player:
-	def __init__(self,name,hp,mp,atk,de,skill,exp,level,items,position,state,money):
+	def __init__(self, name, hp, mp, atk, de,skill,exp,level,items,position,state,money):
 		self.name = name
 		self.hp = hp
 		self.mp = mp
@@ -36,7 +37,7 @@ Enemy_list = [Goblin,LegEater,QueenBee]
 #start menu
 import time
 import copy
-import Rpg_stage 
+from Rpg_stage import *
 import random 
 
 from Rpg_betsys import bet
@@ -44,24 +45,26 @@ from Rpg_betsys import bet_enc
 from Rpg_move import mov  
 
 print('Welocome to Final Fantasy!!!')	
-time.sleep(2)
+time.sleep(0.4)
 raw_input('enter any key to start ')
 
-time.sleep(2)						
-#创建人物			
-Player1 = Player(raw_input('Please enname your warrior : '),30,5,4,0,[],0,1,[],[0,0],'',0)                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+time.sleep(0.4)						
+#创建人物			 
+Player1 = Player(raw_input('Please enname your warrior : '), 30, 5, 4, 0, [], 0, 1, [], [0, 0], '', 0)                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
-stage.scipt1(Player1.name)
+scipt1(Player1.name)
 
 while True:
 	pos_temp0 = copy.deepcopy(Player1.position)
-	pos_temp1,Player1.items,Player1.money = copy.deepcopy(mov(Player1))
+	pos_temp1, Player1.items, Player1.money = copy.deepcopy(mov(Player1))
 	if pos_temp0 != pos_temp1:
 		index = bet_enc(Player1)
 		if index != '':
 			print(Enemy_list[index].name+'出现了！！')
 			time.sleep(1)
-			Player1.state=bet(Player1,Enemy_list[index])
+			Player1.state = bet(Player1,Enemy_list[index])
 			time.sleep(1)
 			Player1.exp += Enemy_list[index].exp
 			Player1.money += Enemy_list[index].money
+
+	    
